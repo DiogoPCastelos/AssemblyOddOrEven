ddd48fac8538d39bcf4d0c154484e4b413fa31b7
Despan Alex <gargant@LAPTOP-450TNJ1O.localdomain>
2022-09-09 15:05:07 +0200

    actual working program now yay

--- powerB.s
+++ powerB.s
@@ -2,9 +2,9 @@
 
 prompt: .asciz "---=== Welcome to Alex trying to learn assembly! ===---\n"
 input_prompt: .asciz "Please (kindly) enter a number and I'll tell you whether it is even or odd!\n"
-input: .asciz "%d"
-output_odd: .asciz "Number %d is ODD!"
-output_even: .asciz "Number %d is EVEN!"
+input: .asciz "%ld"
+output_odd: .asciz "Number %ld is ODD!\n"
+output_even: .asciz "Number %ld is EVEN!\n"
 
 .global main
 
@@ -15,55 +15,62 @@ main:
 	mov %rsp, %rbp     # stack pointer value to base pointer
 	
 	# preparing to show prompt
-	mov $0, %rax       # copying 0 to rax because printf should not take varargs
-	mov $prompt, %rdi  # set the parameter in rdi (for printf)
+	movq $0, %rax       # copying 0 to rax because printf should not take varargs
+	movq $prompt, %rdi  # set the parameter in rdi (for printf)
 	call printf        # calling function to print welcome prompt
 
 	call read_and_check
 
 	# CRAY CRAY EPILOGUE (the end people)
-	mov %rbp,  %rsp    # clearing the stack
-	pop %rbp           # restoring base pointer location
+	movq %rbp,  %rsp    # clearing the stack
+	popq %rbp           # restoring base pointer location
 
 end:
 	# exit code
-	mov $0, %rdi       # setting the exit code (kinda like return 0 in c++)
+	movq $0, %rdi       # setting the exit code (kinda like return 0 in c++)
 	call exit
 
 read_and_check:
 	# again the stupid prologue
-	push %rbp          # base pointer pushing for the new function
-	mov %rsp, %rbp     # stack pointer value to base pointer
+	pushq %rbp          # base pointer pushing for the new function
+	movq %rsp, %rbp     # stack pointer value to base pointer
 	
-	mov $0, %rax       # again, printf will not take any varargs
-	mov $input_prompt, %rdi # first parameter, the input prompt
+	movq $0, %rax       # again, printf will not take any varargs
+	movq $input_prompt, %rdi # first parameter, the input prompt
 	call printf        # calling the function
 	
-	sub $16, %rsp      # reserve space in the stack for input
+	subq $16, %rsp      # reserve space in the stack for input
 			   # (in assembly memory is manually allocated)
-	mov $0, %rax       # again, scanf will just take one argument
-	mov $input, %rdi   # first parameter, the input
-	lea -16(%rbp), %rsi# second parameter, the address of the reserved space
+	movq $0, %rax       # again, scanf will just take one argument
+	movq $input, %rdi   # first parameter, the input
+	leaq -16(%rbp), %rsi# second parameter, the address of the reserved space
 			   # (-16 because I moved it 16 bits up)
 	call scanf
 
-	mov -16(%rbp), %rsi# load the input value into RSI (second param register)
+	movq -16(%rbp), %rsi# load the input value into RSI (second param register)
 	
-	mov %rsi, %rax     # copy input to rax
-	mov $2, %rcx       # move value 2 to rcx (so we can divide later)
-	mov $0, %rdx       # clear the contents of rdx
-	div %rcx           # divide the content of rax by rcx
+	movq %rsi, %rax     # copy input to rax
+	movq $2, %rcx       # move value 2 to rcx (so we can divide later)
+	movq $0, %rdx       # clear the contents of rdx
+	divq %rcx           # divide the content of rax by rcx
 			   # (result SHOULD BE? stored in RAX and the % in RDX)
-	cmp $0, %rdx       # compare rdx to 0
+	cmpq $0, %rdx       # compare rdx to 0
 	jne odd            # JUMP NOT EQUAL to odd
 
 even: 
-	mov $0, %rax       # no varargs for prinft
-	mov $output_even, %rdi # param 1 output string
+	movq $0, %rax
+	movq $output_even, %rdi
 	call printf
-	ret
+	
+	jmp clear_stack
 odd:
-	mov $0, %rax       # no varargs for printf
-	mov $output_odd, %rdi # param 1 output string
+	movq $0, %rax       # no varargs for printf
+	movq $output_odd, %rdi # param 1 output string
 	call printf
+	
+	jmp clear_stack
+clear_stack:
+	movq %rbp, %rsp
+	popq %rbp
+	
 	ret
